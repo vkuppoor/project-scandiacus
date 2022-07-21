f42c72c71fa34647662694f02284b0fbc883319a
rkrzywda <50608358+rkrzywda@users.noreply.github.com>
2022-07-20 23:15:10 -0400

    basic fast api implementation

---
+++ api/fast_api_app/main.py
@@ -0,0 +1,22 @@
+import os
+from collections import defaultdict
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+
+
+from . import crud, models, schemas
+
+app = FastAPI()
+
+@app.get("/test", tags = ["testing"],)
+def test() -> dict:
+    """Tests the api to make sure it is running.
+    
+    Args:
+        None
+
+    Returns:
+        "Hello World"
+    """
+    return "Hello World"
