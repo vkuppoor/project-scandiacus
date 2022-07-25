518cdf7734527f2fbc3352bd99ed8de99ada49bf
rkrzywda <50608358+rkrzywda@users.noreply.github.com>
2022-07-24 22:41:05 -0400

    learn to commit on windows terminal

--- api/fast_api_app/crud.py
+++ api/fast_api_app/crud.py
@@ -4,6 +4,8 @@ def add_YOLO_annotation(path: str, label: str, coords: tuple) -> str:
 
     with open(path + ".txt", 'a') as f:
         cx, cy, w, h = functions.convert_coordinates_to_YOLO(path, coords)
+
+        class_number = label
         annotation = f"CLASS! {cx} {cy} {w} {h} \n"
         #class number is determined by the order the labels are made
         #so whenever a user makes a new label the number goes up one
