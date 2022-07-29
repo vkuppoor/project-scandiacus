a04e995557b0f05228f6ac4308f239780cdb2036
rkrzywda <50608358+rkrzywda@users.noreply.github.com>
2022-07-28 23:18:54 -0400

    labelmap functions done, ready to test

--- api/fast_api_app/crud.py
+++ api/fast_api_app/crud.py
@@ -9,14 +9,7 @@ def add_YOLO_annotation(path: str, label: str, coords: tuple) -> str:
 
     with open(path + ".txt", 'a') as f:
         cx, cy, w, h = functions.convert_coordinates_to_YOLO(path, coords)
-
-        #In YOLO there are 5 numbers, one for the classification. The number is 
-        #mapped to 
-        annotation = f"{len(labels)} {cx} {cy} {w} {h} \n"
-        #class number is determined by the order the labels are made
-        #so whenever a user makes a new label the number goes up one
-        #not too hard but reinforces my belief that we need a db to store annotations
-        print(annotation)
+        annotation = f"{len(labels) - 1} {cx} {cy} {w} {h} \n"
         f.write(annotation)
     return annotation
 
--- api/fast_api_app/labelmap.py
+++ api/fast_api_app/labelmap.py
@@ -11,16 +11,36 @@ def add_to_label_map(label, labels):
     Returns:
         None
     """
+    if(correct_label_map(labels)):
+        with open("labelmap.txt", 'a') as f:
+            text = f"{len(labels) - 1} {label} \n"
+            f.write(text)
+    else: 
+        delete_label_map()
+        i = 0
+        for label in labels:
+            with open("labelmap.txt", 'a') as f:
+                text = f"{i} {label} \n"
+                f.write(text)
+                i += 1
 
-    #What if the person shuts the file, all of the labels are gone, but not the 
-    #text file. Might cause some issues with adding new labels.
-    #NEED TO CHECK if the length of the txt file is abnormally larger than the
-    #list, if it is then I need to match it again to the list
-    with open("labelmap.txt", 'a') as f:
-        text = f"{len(labels)} {label} \n"
-        f.write(text)
 
-def delete_from_label_map(label, labels):
+def correct_label_map(labels):
+    """Function to ensure that the labels list and labelmap file match.
+
+    Args:
+        labels: list (contains all of the labels selected by the user)
+    
+    Returns:
+        None
+    """
+    with open("labelmap.txt", 'r') as f:
+        lines = f.readlines()
+    if(len(labels) != len(lines)):
+        return False
+    return True
+
+def delete_label_from_label_map(label, labels):
     """Deletes a specific label from a labelmap
     Enables the user to figure out which number belongs to which class.
     
@@ -30,18 +50,13 @@ def delete_from_label_map(label, labels):
     Returns:
         None
     """
-
-    #What if the person shuts the file, all of the labels are gone, but not the 
-    #text file. Might cause some issues with adding new labels.
-    #NEED TO CHECK if the length of the txt file is abnormally larger than the
-    #list, if it is then I need to match it again to the list
     with open("labelmap.txt", 'r') as f:
         lines = f.readlines()
     
     i = 0
     while i < len(lines):
         if(label in lines[i]):
-            lines[i].pop()
+            lines[i].pop() #this is done to avoid popping a list in a for loop
         else:
             i+=1
             words = lines[i].split()
@@ -50,11 +65,6 @@ def delete_from_label_map(label, labels):
 
     with open("labelmap.txt", 'w') as f:
         f.writelines(lines)
-
-
-def correct_label_map(labels):
-    #make sure that it aligns with labels
-    print("IDK WHAT TO DO")
 
 def delete_label_map():
     """Delete the labelmap file.
