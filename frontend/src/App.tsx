5c5146ce01a5a29c092a0f417d3507ccf8ab07d7
Venkat_Kuppoor <venkkuppoor@gmail.com>
2022-08-03 12:51:48 -0400

    TypeScript set up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './assets/css/App.css';
+import './assets/css/app.css'
 import FileManager from './components/FileManager';
 
 function App() {
