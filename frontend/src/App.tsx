6ba9a4ce002ea520478d3af06a97fa3101240b38
Venkat_Kuppoor <venkkuppoor@gmail.com>
2022-08-05 16:23:06 -0400

    Implemented PostCSS + TailwindCSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './assets/css/app.css'
+// import './assets/css/app.css'
+import './App.css'
 import FileManager from './components/FileManager';
 
 function App() {
   return (
-      <div className="main-grid">
-        <div className="component-col left-sidebar">
+      <div id="main-grid" className="grid grid-cols-4 h-screen">
+        <div className="sidebar">
           <FileManager />
         </div>
-        <div className="component-col main-panel"></div>
-        <div className="component-col right-sidebar"></div>
+        <div className="col-span-2"></div>
+        <div className="sidebar"></div>
       </div>
     );
 }
