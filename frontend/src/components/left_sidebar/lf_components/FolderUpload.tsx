c1f195977b514dee00d396a18968a693d6dc32da
Venkat_Kuppoor <venkkuppoor@gmail.com>
2022-08-06 08:57:47 -0400

    Added supported files for folder dropzone

--- frontend/src/components/left_sidebar/lf_components/FolderUpload.tsx
+++ frontend/src/components/left_sidebar/lf_components/FolderUpload.tsx
@@ -4,7 +4,23 @@ import { Container } from "reactstrap";
 import { useDropzone, FileWithPath } from "react-dropzone";
 
 const FolderUpload = () => {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const [files, setFiles] = React.useState();
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      "image/jpeg": [".jpeg", ".jpg"],
+      "image/png": [".png"],
+      "image/bmp": [".bmp"],
+      "image/tif": [".tif", ".tiff"],
+      "image/dng": [".dng"],
+      "image/webp": [".webp"],
+      "image/mpo": [".mpo"],
+      "text/html": [".html", ".htm"],
+      "application/json": [".json"],
+      "application/xml": [".xml"],
+      "text/plain": [".txt"],
+    },
+  });
 
   // const files = acceptedFiles.map(file => (
   //     <li key={file.path}>
@@ -13,9 +29,9 @@ const FolderUpload = () => {
   // ));
 
   return (
-    <Container className="dropzone | flex flex-col items-center">
+    <Container className="dropzone | flex flex-col items-center bg-white">
       <input
-        className="folder-upload | w-full hidden"
+        className="folder-upload | w-full"
         type="file"
         // webkitdirectory=""
         directory=""
