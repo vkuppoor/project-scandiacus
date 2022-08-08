e87f74a832326627cd04cd2f48cdac2c31704415
Praneeth Uppari <praneethuppari@vt.edu>
2022-08-08 14:53:26 -0400

    Added two coco_mock files. One with attributes. One that has the essentials.

--- frontend/src/components/FileManager.tsx
+++ frontend/src/components/FileManager.tsx
@@ -2,11 +2,11 @@ import React, { useState, useRef, useEffect } from "react";
 import { Container } from "reactstrap";
 
 const FileManager = () => {
-  return (
-    <Container className="file-manager-sidebar">
-      <h1>File Manager Sidebar</h1>
-    </Container>
-  );
+    return (
+        <Container className="file-manager-sidebar">
+            <h1>File Manager Sidebar</h1>
+        </Container>
+    );
 };
 
 export default FileManager;
